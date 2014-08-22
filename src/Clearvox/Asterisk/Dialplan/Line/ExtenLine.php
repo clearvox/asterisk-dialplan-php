59f0054e617054b3828e3008147ac82fac4dea40
Leon Rowland <leon@clearvox.nl>
2014-08-22 11:14:05 +0200

    comments

--- src/Clearvox/Asterisk/Dialplan/Line/ExtenLine.php
+++ src/Clearvox/Asterisk/Dialplan/Line/ExtenLine.php
@@ -27,21 +27,43 @@ class ExtenLine implements LineInterface
         $this->application = $application;
     }
 
+    /**
+     * Get the pattern for this line. There is no guarantee that
+     * the response string wouldn't be empty.
+     *
+     * @return string
+     */
     public function getPattern()
     {
         return $this->pattern;
     }
 
+    /**
+     * Get the priority for this line. There is no guarantee that
+     * the response string wouldn't be empty.
+     *
+     * @return string
+     */
     public function getPriority()
     {
         return $this->priority;
     }
 
+    /**
+     * Get the application associated with this line.
+     *
+     * @return \Clearvox\Asterisk\Dialplan\Application\ApplicationInterface
+     */
     public function getApplication()
     {
         return $this->application;
     }
 
+    /**
+     * Turn this implemented Line into a string representation.
+     *
+     * @return string
+     */
     public function toString()
     {
         return 'exten => ' .
