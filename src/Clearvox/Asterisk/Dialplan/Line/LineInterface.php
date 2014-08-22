a0f30f09bf2e9aca3b52ac884a49c429b12e0a77
Leon Rowland <leon@clearvox.nl>
2014-08-22 10:19:46 +0200

    Comments

--- src/Clearvox/Asterisk/Dialplan/Line/LineInterface.php
+++ src/Clearvox/Asterisk/Dialplan/Line/LineInterface.php
@@ -3,14 +3,33 @@ namespace Clearvox\Asterisk\Dialplan\Line;
 
 interface LineInterface
 {
+    /**
+     * Get the pattern for this line. There is no guarantee that
+     * the response string wouldn't be empty.
+     *
+     * @return string
+     */
     public function getPattern();
 
+    /**
+     * Get the priority for this line. There is no guarantee that
+     * the response string wouldn't be empty.
+     *
+     * @return string
+     */
     public function getPriority();
 
     /**
+     * Get the application associated with this line.
+     *
      * @return \Clearvox\Asterisk\Dialplan\Application\ApplicationInterface
      */
     public function getApplication();
 
+    /**
+     * Turn this implemented Line into a string representation.
+     *
+     * @return string
+     */
     public function toString();
 }
