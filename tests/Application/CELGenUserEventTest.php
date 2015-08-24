2bccabb7a228252f606f8bac5d22d998e15155ea
Leon Rowland <leon@clearvox.nl>
2015-08-24 14:19:27 +0200

    CELGenUserEvent application

---
+++ tests/Application/CELGenUserEventTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use Clearvox\Asterisk\Dialplan\Application\CELGenUserEvent;
+
+class CELGenUserEventTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CELGenUserEvent
+     */
+    protected $genUserEvent;
+
+    public function setUp()
+    {
+        $this->genUserEvent = new CELGenUserEvent('testing');
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals('CELGenUserEvent', $this->genUserEvent->getName());
+    }
+
+    public function testGetEventName()
+    {
+        $this->assertEquals('testing', $this->genUserEvent->getEventName());
+    }
+
+    public function testGetDataWithNoExtra()
+    {
+        $this->assertEquals('testing', $this->genUserEvent->getData());
+    }
+
+    public function testGetDataWithExtra()
+    {
+        $this->genUserEvent->setExtra('0123456789');
+        $this->assertEquals('testing,0123456789', $this->genUserEvent->getData());
+    }
+
+    public function testToArray()
+    {
+        $this->genUserEvent->setExtra('0123456789');
+
+        $expected = [
+            'event_name' => 'testing',
+            'extra' => '0123456789'
+        ];
+
+        $this->assertEquals($expected, $this->genUserEvent->toArray());
+    }
+
+    public function testToJson()
+    {
+        $this->genUserEvent->setExtra('0987654321');
+
+        $expected = json_encode([
+            'event_name' => 'testing',
+            'extra' => '0987654321'
+        ]);
+
+        $this->assertEquals($expected, $this->genUserEvent->toJson());
+    }
+}
