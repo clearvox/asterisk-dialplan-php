2f000e64c2dfe5d6dd5506bb98e30537d5b926df
Filipe Oliveira <filip3oliveira@gmail.com>
2025-02-06 13:49:25 +0000

    UC-7605 Update code and add to top line dialplan feature

--- src/Clearvox/Asterisk/Dialplan/Line/CommentLine.php
+++ src/Clearvox/Asterisk/Dialplan/Line/CommentLine.php
@@ -1,11 +1,13 @@
 <?php
 namespace Clearvox\Asterisk\Dialplan\Line;
 
+use Clearvox\Asterisk\Dialplan\Application\ApplicationInterface;
+
 class CommentLine implements LineInterface
 {
     protected $comment;
 
-    public function __construct($comment)
+    public function __construct(string $comment)
     {
         $this->comment = $comment;
     }
@@ -13,10 +15,8 @@ class CommentLine implements LineInterface
     /**
      * Get the pattern for this line. There is no guarantee that
      * the response string wouldn't be empty.
-     *
-     * @return string
      */
-    public function getPattern()
+    public function getPattern(): string
     {
         return '';
     }
@@ -24,31 +24,25 @@ class CommentLine implements LineInterface
     /**
      * Get the priority for this line. There is no guarantee that
      * the response string wouldn't be empty.
-     *
-     * @return string
      */
-    public function getPriority()
+    public function getPriority(): ?string
     {
-        return '';
+        return null;
     }
 
     /**
      * Get the application associated with this line.
-     *
-     * @return \Clearvox\Asterisk\Dialplan\Application\ApplicationInterface
      */
-    public function getApplication()
+    public function getApplication(): ?ApplicationInterface
     {
         return null;
     }
 
     /**
      * Turn this implemented Line into a string representation.
-     *
-     * @return string
      */
-    public function toString()
+    public function toString(): string
     {
-        return "; {$this->comment}";
+        return "; $this->comment";
     }
 }
